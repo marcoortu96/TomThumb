eddeedb5e6864bc7698b6f39a5d6b5a053799e72
andreaRe97 <a.re@studenti.unica.it>
2020-07-13 17:33:19 +0200

    Aggiunta Pods e MapPin

---
+++ TomThumb/Map/MapPin.swift
@@ -0,0 +1,22 @@
+//
+//  MapPin.swift
+//  TomThumb
+//
+//  Created by Andrea Re on 13/07/2020.
+//  Copyright © 2020 Sora. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class MapPin : NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+
+    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+}
